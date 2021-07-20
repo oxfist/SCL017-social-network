4d3db72b876334009c7f7d7921cfca88896d0d3f
Andrés Quilodrán <andres.quilodran@laboratoria.la>
2021-07-20 17:57:40 -0400

    Add insertion of Home content by default and usage of replaceState to properly display routes without '#'

--- src/main.js
+++ src/main.js
@@ -2,16 +2,24 @@
 //   1.1 Reemplazar contenido de div con id root
 //   1.2 Cambiar URL para que no ocupe el '#'
 // 2. Mostrar HTML correcto al recargar la página
+// 3. Mostrar HTML correcto al moverse con botones Atrás y Adelante del navegador
 
 const rootDiv = document.getElementById('root');
 
+const currentPath = window.location.pathname;
+if (currentPath === '/') {
+  rootDiv.innerHTML = '<h1>Inicio</h1>';
+}
+
 const changeRoute = (hash) => {
   if (hash === '#home') {
-    rootDiv.innerHTML = '<h1>Inicio</h1>';
     console.log('mostrar home');
+    rootDiv.innerHTML = '<h1>Inicio</h1>';
+    window.history.replaceState({}, 'home', '/');
   } else if (hash === '#about') {
-    rootDiv.innerHTML = '<h1>Acerca de</h1>';
     console.log('mostrar about');
+    rootDiv.innerHTML = '<h1>Acerca de</h1>';
+    window.history.replaceState({}, 'about', '/about');
   }
 };
 
