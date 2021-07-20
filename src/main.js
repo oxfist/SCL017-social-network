2d23a5c0647fbdd4c66c06b045b40abf70492a82
Andrés Quilodrán <andres.quilodran@laboratoria.la>
2021-07-20 12:36:44 -0400

    Add 1.1: replacing HTML content for root div when selecting link (hashchange event)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+// 1. Mostrar HTML después de seleccionar un link (navegación)
+//   1.1 Reemplazar contenido de div con id root
+//   1.2 Cambiar URL para que no ocupe el '#'
+// 2. Mostrar HTML correcto al recargar la página
+
+const rootDiv = document.getElementById('root');
+
 const changeRoute = (hash) => {
-  console.log(`cambiar a ruta seleccionada ${hash}`);
+  if (hash === '#home') {
+    rootDiv.innerHTML = '<h1>Inicio</h1>';
+    console.log('mostrar home');
+  } else if (hash === '#about') {
+    rootDiv.innerHTML = '<h1>Acerca de</h1>';
+    console.log('mostrar about');
+  }
 };
 
 window.addEventListener('hashchange', () => {
