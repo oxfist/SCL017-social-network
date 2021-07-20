4e7ed2e54ea8178129ddbba28d8061a5a5799257
Andrés Quilodrán <andres.quilodran@laboratoria.la>
2021-07-20 18:06:29 -0400

    Add 2: showing the proper HTML content when reloading the page

--- src/main.js
+++ src/main.js
@@ -6,19 +6,24 @@
 
 const rootDiv = document.getElementById('root');
 
+const homeContent = '<h1>Inicio</h1>';
+const aboutContent = '<h1>Acerca de</h1>';
+
 const currentPath = window.location.pathname;
 if (currentPath === '/') {
-  rootDiv.innerHTML = '<h1>Inicio</h1>';
+  rootDiv.innerHTML = homeContent;
+} else if (currentPath === '/about') {
+  rootDiv.innerHTML = aboutContent;
 }
 
 const changeRoute = (hash) => {
   if (hash === '#home') {
     console.log('mostrar home');
-    rootDiv.innerHTML = '<h1>Inicio</h1>';
+    rootDiv.innerHTML = homeContent;
     window.history.replaceState({}, 'home', '/');
   } else if (hash === '#about') {
     console.log('mostrar about');
-    rootDiv.innerHTML = '<h1>Acerca de</h1>';
+    rootDiv.innerHTML = aboutContent;
     window.history.replaceState({}, 'about', '/about');
   }
 };
