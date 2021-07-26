b3e088e55982c4efedc9e44a4012f00b065756de
Andrés Quilodrán <andres.quilodran@laboratoria.la>
2021-07-26 08:44:25 -0400

    Extracted HTML setting from changeRoute and added comments to better depict code intention

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const rootDiv = document.getElementById('root');
 const homeContent = '<h1>Inicio</h1>';
 const aboutContent = '<h1>Acerca de</h1>';
 
+// Mostrar HTML correcto al recargar página (2)
 const currentPath = window.location.pathname;
 if (currentPath === '/') {
   rootDiv.innerHTML = homeContent;
@@ -16,22 +17,28 @@ if (currentPath === '/') {
   rootDiv.innerHTML = aboutContent;
 }
 
+// Cambiar ruta en la URL para que no use "#", ej. "/#about" por "/about" (1.2)
 const changeRoute = (hash) => {
   if (hash === '#home') {
-    console.log('mostrar home');
-    rootDiv.innerHTML = homeContent;
     window.history.replaceState({}, 'home', '/');
   } else if (hash === '#about') {
-    console.log('mostrar about');
-    rootDiv.innerHTML = aboutContent;
     window.history.replaceState({}, 'about', '/about');
   }
 };
 
+// Cambiar HTML al clickear links (1)
 window.addEventListener('hashchange', () => {
   changeRoute(window.location.hash);
+  if (hash === '#home') {
+    console.log('mostrar home');
+    rootDiv.innerHTML = homeContent;
+  } else if (hash === '#about') {
+    console.log('mostrar about');
+    rootDiv.innerHTML = aboutContent;
+  }
 });
 
+// Manejar botones Atrás y Adelante (3)
 window.onpopstate = () => {
   if (window.location.pathname === '/') {
     rootDiv.innerHTML = homeContent;
